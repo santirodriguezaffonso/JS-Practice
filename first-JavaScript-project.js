4d439132aca424f373b31e6e6668840729662878
Santiago Rodriguez Affonso <sraffonso@itcrowdarg.com>
2022-10-10 11:47:14 -0300

    Complete list exercise

--- first-JavaScript-project.js
+++ first-JavaScript-project.js
@@ -1,1 +1,1 @@
-
+var myVariable = "newVar";
---
+++ listExercise.js
@@ -0,0 +1,11 @@
+const birthdayDate = new Date(1997, 01, 14);
+const favoriteBook = {
+  title: "Economics in One Lesson",
+  author: "Henry Hazlitt",
+  date: new Date(1978),
+  url: new URL(
+    "https://www.amazon.com/Economics-One-Lesson-Shortest-Understand/dp/0517548232/ref=nodl_?dplnkId=a02c804b-fe09-4618-80b0-c5d5fb0e375e"
+  ),
+};
+
+const list = [Santiago, 25, true, birthdayDate, favoriteBook];
